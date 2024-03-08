83458b3f4eb5e0df67b3c8f443ee4e79f7eb80c8
onlymtt <162594554+onlymtt@users.noreply.github.com>
2024-03-08 17:50:10 +0100

    Update Auto.java

--- Concessionaria/src/otherPackage/Auto.java
+++ Concessionaria/src/otherPackage/Auto.java
@@ -4,4 +4,27 @@ public class Auto {
   int price;
   String carName;
 
+  
+
+
+  public Auto(int price, String carName) {
+    this.price = price;
+    this.carName = carName;
+  }
+  public int getPrice() {
+    return price;
+  }
+  public String getCarName() {
+    return carName;
+  }
+  public void setPrice(int price) {
+    this.price = price;
+  }
+  public void setCarName(String carName) {
+    this.carName = carName;
+  }
+
+  
+  
+
 }
